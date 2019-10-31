c7272a16e9925e91846854927f6fd4dde778e2a3
Glitch (itscodenation-rockpaperscissors) <none>
2019-10-31 00:19:35 +0000

    🚌🏩 Checkpoint ./js/script.js:3247120/1692

--- js/script.js
+++ js/script.js
@@ -5,22 +5,45 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+  var moves = ["rock","paper","scissors"]
+  var numberX = Math.random();
+  var numberY = numberX* 3
+  var numberN = Math.floor(numberY);
+  console.log(numberN)
+
 $("#shoot").click(function(){
   let playerInput = $("#input").val();
   var playerMove = playerInput.toLowerCase();
-  botMove();
   
   if (playerMove === "rock"){
-      if (numberN = "rokc")
+    
+      if (numberN === "rock"){
+        $("#result").append("Tie!")
+      }else if (numberN === "paper"){
+        $("#result").append("Bot Wins!")
+      }else if (numberN === "scissors"){
+        $("#result").append("Player Wins!")
       }
+  
+  }else if (playerMove === "paper"){
+    
+    if (numberN === "rock"){
+        $("#result").append("Player Wins!")
+      }else if (numberN === "paper"){
+        $("#result").append("Tie!")
+      }else if (numberN === "scissors"){
+        $("#result").append("Bot Wins!")
+      }
+  }else if (playerMove === "scissors")
+        $("#result").append("Player Wins!")
+      }else if (numberN === "paper"){
+        $("#result").append("Player Wins!")
+      }else if (numberN === "scissors"){
+        $("#result").append("Bot Wins!")
+      }
+  }
+  
 });
-
-function botMove(){
-var moves = ["rock","paper","scissors"]
-var numberX = Math.random();
-var numberY = numberX* 3
-var numberN = Math.floor(numberY);
-}
 
 // DOCUMENT READY FUNCTION BELOW
 
